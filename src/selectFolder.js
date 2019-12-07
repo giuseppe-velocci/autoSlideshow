ffb2f0c62743092ef2464ff8ca1e51b9afa42446
Sara Iacobelli <sara88.iacobelli@gmail.com>
2019-12-07 18:02:50 +0100

    fix folder selection

---
+++ src/selectFolder.js
@@ -0,0 +1,18 @@
+const testFolder =  __dirname+'/categories/';
+const fs = require('fs');
+let list="Select the folder:  <select id='scelta'>";
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+	  if(fs.lstatSync(testFolder + file).isDirectory())
+		list = list + " <option value="+ file +">"+ file + "</option>"
+	});
+list = list + "</select>";
+document.getElementById("folder").innerHTML = list;
+});
+
+function save()
+{
+	let sceltaIndex = document.getElementById("scelta");
+	sessionStorage.setItem("animationFolder", sceltaIndex[sceltaIndex.selectedIndex].value);
+	document.getElementById("animazione").submit();
+}
