37894e5980fe835230e54873d268f5c7c3161d4c
Giuseppe Velocci <runnermean@gmail.com>
2019-12-18 23:55:56 +0100

    main color per object

--- src/ts-img-action.js
+++ src/ts-img-action.js
@@ -66,6 +66,47 @@ const getMainColor = (rgb) => {
   return mainColor;
 }
 
+// get main color by bounding box
+const getMainColorByBoundingBox = (rgb, bx, by, bw, bh, width, height) => {
+  const mainColor = {r:0, g:0, b:0};
+  const imgColors = {c:[], max:{val:0, freq:0}};
+  bx = Math.round(bx);
+  by = Math.round(by);
+
+//  console.log(bx, by, bw, bh);
+//  console.log(rgb.length);
+
+  // start = ox
+  const startx = NUMBER_OF_CHANNELS * (bx + by * width);
+  const endx = startx + width * NUMBER_OF_CHANNELS;
+  let stepx, id;
+
+  for (let j = 0; j < bh; j++) {
+    stepx = width * j * NUMBER_OF_CHANNELS;
+    for (let i = startx; i < endx; i += 3) {
+      id = i + stepx;
+      index = ''+rgb[id]+'.'+rgb[id +1]+'.'+rgb[id +2];
+      if (imgColors.c[index] == undefined) {
+        imgColors.c[index] = 1;
+      }else{
+        imgColors.c[index]++;
+      }
+      if (imgColors.c[index] > imgColors.max.freq) {
+        imgColors.max.val  = index;
+        imgColors.max.freq = imgColors.c[index]; 
+      }
+    }
+  }
+ // console.log(imgColors);
+  const res = imgColors.max.val.split('.');
+  mainColor.r = res[0];
+  mainColor.g = res[1];
+  mainColor.b = res[2];
+
+  return mainColor;
+}
+
+
 // function that checks extension for images to ensure ONLY jpg will be parsed
 const correctImgExtension = (imagePath, ext) => {
   let splitPath = path.parse(imagePath);
@@ -86,14 +127,27 @@ async function detect (imagePath) {
     if (correctImgExtension (imagePath, ['jpg', 'jpeg'])) {
       return {};
     }
-    // console.log(imagePath);
     
     const image = readImage(imagePath);
     const input = imageToInput(image, NUMBER_OF_CHANNELS);
   
     const model = await ccsd.load();
     const detection = await model.detect(input.t3d);
+
+    // color detection
     input.mainColor = getMainColor(input.rgb);
+    console.log(detection);
+    for (let i = 0; i < detection.length; i++) {
+      detection[i]['objColor'] = getMainColorByBoundingBox (
+        input.rgb, 
+        detection[i].bbox[0], 
+        detection[i].bbox[1], 
+        detection[i].bbox[2], 
+        detection[i].bbox[3], 
+        input.width, 
+        input.height);
+    }
+
     const result = {detection: detection, width: input.width, height: input.height, mainColor: input.mainColor, image:imagePath};
       console.log('classification results: ', result);
       console.log(imagePath);
