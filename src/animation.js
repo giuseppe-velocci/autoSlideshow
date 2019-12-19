43507274a179e3eb8019f031c8e819afeda48163
Giuseppe Velocci <g.velocci@studenti.local>
2019-12-19 13:02:22 +0100

    bbox feat

--- src/animation.js
+++ src/animation.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
 	let end = "<div id='end' style='display: none; font-size: xx-large; color: #808080;'>THE END </div>";
 
 	$.getJSON(folder + "/detect.json", function(r){
-
 		let i=0;
 		presentation(i, r);	
 		let animation = setInterval(() => {
@@ -28,14 +27,31 @@ $(document).ready(function() {
 	function presentation(i, r)
 	{
 		let img = "<div id='divfoto' style='background:black; overflow:hidden; position:absolute; width:" + r.dati[i].width + "px; height:" + r.dati[i].height +"px'>";
-		img = img + "<img id='foto' src=" + folder + "/" + r.dati[i].file + " style='position:relative;' ></div>"
+		img = img + "<img id='foto' src=" + folder + "/" + r.dati[i].file + " style='position:relative;' >"
 		img = img + end;
+		const o = r.dati[i];
+		for (let k=0; k < o.data.length; k++) {
+			console.log(o.data[k]);
+			img += "<div class='bbox' style='position:absolute; border:solid; border-color:rgb("
+			+ (+255 -o.data[k].objColor.r) + "," + (+255 -o.data[k].objColor.g) + "," + (+255 -o.data[k].objColor.b) 
+			+"); border-width:3px;"
+			+ "left:"+ o.data[k].bbox[0]
+			+ "px; top:"+ o.data[k].bbox[1]
+			+"px; width:"+ o.data[k].bbox[2]
+			+"px; height:"+ o.data[k].bbox[3]
+			+"px; "
+			+"px; z-index:9999;'></div>";
+		}
+		img += "</div>";
 
-	//	$("#colorbar").css('color', `rgb(${r.dati[i].mainColor.r},${r.dati[i].mainColor.g},${r.dati[i].mainColor.b})`);
-		$("#colorbar").css('color', `rgb(${r.dati[i].data[0].objColor.r},${r.dati[i].data[0].objColor.g},${r.dati[i].data[0].objColor.b})`);
+	$("#colorbar").css('color', `rgb(${r.dati[i].mainColor.r},${r.dati[i].mainColor.g},${r.dati[i].mainColor.b})`);
+	//	$("#colorbar").css('color', `rgb(${r.dati[i].data[0].objColor.r},${r.dati[i].data[0].objColor.g},${r.dati[i].data[0].objColor.b})`);
 
 		$("#animationDiv").html(img);
+
+		$(".bbox").animate({'opacity': '0'}, 1500);
 		
+		setTimeout(() => {
 		let p = interestArea.interestArea(r.dati[i].data, r.dati[i].width, r.dati[i].height);
 		$("#foto").animate(
 			//left: -left, top: -up
@@ -57,5 +73,6 @@ $(document).ready(function() {
 			, 'left': `${(window.innerWidth /2 -(r.dati[i].width-p.left-p.right))}px`
 			}, 3000
 		);
+	}, 1500);
 	}
 }); 
